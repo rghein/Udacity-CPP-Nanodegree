c17b4307c19d64aeee9d21915b38e2257329702d
rghein <=>
2019-07-24 15:22:24 -0500

    Lesson Two

---
+++ Concurrency/Lesson Two/Promises And Futures/example_3.cpp
@@ -0,0 +1,43 @@
+#include <cmath>
+#include <future>
+#include <iostream>
+#include <thread>
+
+void computeSqrt(std::promise<double> &&prms, double input) {
+  std::this_thread::sleep_for(
+      std::chrono::milliseconds(2000));  // simulate work
+      double output = sqrt(input);
+      prms.set_value(output);
+}
+
+int main() {
+  // define input data
+  double inputData = 2.0;
+
+  // create promise and future
+  std::promise<double> prms;
+  std::future<double> ftr = prms.get_future();
+
+  // start thread and pass promise as argument
+  std::thread t(computeSqrt, std::move(prms), inputData);
+
+  // Student task STARTS here
+  // wait for result to become available
+  auto status = ftr.wait_for(std::chrono::milliseconds(1000));
+  // if result is ready
+  if (status == std::future_status::ready)  {
+    std::cout << "Result = " << ftr.get() << std::endl;
+  }
+
+  //  timeout has expired or function has not yet been started
+  else if (status == std::future_status::timeout ||
+           status == std::future_status::deferred) {
+    std::cout << "Result unavailable" << std::endl;
+  }
+  // Student task ENDS here
+
+  // thread barrier
+  t.join();
+
+  return 0;
+}
---
+++ Concurrency/Lesson Two/Promises And Futures/example_4.cpp
@@ -0,0 +1,43 @@
+#include <cmath>
+#include <future>
+#include <iostream>
+#include <memory>
+#include <thread>
+
+void divideByNumber(std::promise<double> &&prms, double num, double denom) {
+  std::this_thread::sleep_for(std::chrono::milliseconds(500));  // simulate work
+
+  try {
+    if (denom == 0)
+      throw std::runtime_error("Exception from thread: Division by zero!");
+    else
+      prms.set_value(num / denom);
+  } 
+  catch (...) {
+    prms.set_exception(std::current_exception());
+  }
+}
+
+int main() {
+  // create promise and future
+  std::promise<double> prms;
+  std::future<double> ftr = prms.get_future();
+
+  // start thread and pass promise as argument
+  double num = 42.0, denom = 6.0;
+  std::thread t(divideByNumber, std::move(prms), num, denom);
+
+  // retrieve result within try-catch-block
+  try {
+    double result = ftr.get();
+    std::cout << "Result = " << result << std::endl;
+  } 
+  catch (std::runtime_error e) {
+    std::cout << e.what() << std::endl;
+  }
+
+  // thread barrier
+  t.join();
+
+  return 0;
+}
